5f0539f032291d7ff40454e6293de6bc93a40550
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2018-04-27 01:20:26 +0200

    atom-vim-like-tab

--- packages.cmd
+++ packages.cmd
@@ -1,1 +1,1 @@
-apm install autocomplete-java no-evil-eol-newline project-manager todo-show process-palette advanced-open-file file-icons vim-mode-plus trailing-spaces vim-mode-plus-keymaps-for-surround
+apm install autocomplete-java no-evil-eol-newline project-manager todo-show process-palette advanced-open-file file-icons vim-mode-plus trailing-spaces vim-mode-plus-keymaps-for-surround atom-vim-like-tab vim-mode-zz
