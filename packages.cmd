45404abd91d85455070add03390f0a83352d170a
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2018-04-26 22:24:35 +0200

    initial import

---
+++ packages.cmd
@@ -0,0 +1,1 @@
+apm install autocomplete-java no-evil-eol-newline project-manager todo-show process-palette advanced-open-file file-icons vim-mode-plus trailing-spaces vim-mode-plus-keymaps-for-surround
